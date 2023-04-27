3ac23ad3d45765dfe5c97e3737b1dd4f77da1ae3
PURUSHOTHAMANSP <131940071+PURUSHOTHAMANSP@users.noreply.github.com>
2023-04-27 00:34:03 -0400

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,34 @@
+
+class Student
+
+   {
+
+    int age;
+    int mark;
+
+    
+
+  public static void main(String arg[])
+     
+    {
+      
+    Student Students=new Student();
+
+     Students.age=21;
+     Students.mark=20;
+     
+
+    System.out.println(Students.age);
+    
+    System.out.println(Students.mark);
+       
+     }
+     
+
+  }
+   
+
+   
+
+
+
